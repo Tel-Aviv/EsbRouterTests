55a02bcbd348b9c6a08df4d9405a745decdd5f9c
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-07-03 19:22:05 +0300

    Update ucp.test.js

--- tests/ucp.test.js
+++ tests/ucp.test.js
@@ -1,11 +1,14 @@
 import webdriver from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
+import chromedriver from 'chromedriver';
 
 describe('Digitel Site', () => {
 
   let driver;
 
   beforeAll( async() => {
+
+    chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 
     driver = await new webdriver.Builder()
              .withCapabilities(webdriver.Capabilities.chrome())
@@ -33,6 +36,10 @@ describe('Digitel Site', () => {
       elem = await driver.findElement(webdriver.By.id('ctl00_ctl65_g_088dab0c_3a75_4d86_a248_4203f4a4c226_ctl00_txtEmail'));
       await elem.clear();
       await elem.sendKeys('oleg_kleyman@hotmail.com');
+
+      elem = await driver.findElement(webdriver.By.id('ctl00_ctl65_g_088dab0c_3a75_4d86_a248_4203f4a4c226_ctl00_txtPhone'));
+      await elem.clear();
+      await elem.sendKeys('039210968');
 
       elem = await driver.findElement(webdriver.By.css('#ctl00_ctl65_g_088dab0c_3a75_4d86_a248_4203f4a4c226_ctl01_pnlButtons > a'));
       await elem.click();
